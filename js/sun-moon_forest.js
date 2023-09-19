278b76d68b132dd0e74da6a077fe008015049ee1
Anka <103377387+ann153476@users.noreply.github.com>
2023-09-19 14:55:46 +0300

    all

--- js/sun-moon_forest.js
+++ js/sun-moon_forest.js
@@ -1,11 +1,35 @@
 const toggleBtnForest = document.querySelector(".sun-moon-forest");
 const bacForest = document.querySelector(".s-m_bac__forest");
+const forestSwitch = document.querySelector(".forest__switch");
 
-function forestOnFunc() {
-  //   isDarkMode = !isDarkMode;
-  //   if (isDarkMode) {
-  //     bacForest.style.opacity = "0";
-  //   } else {
-  bacForest.style.opacity = "0";
+let isDark = localStorage.getItem("isDarkForestBTN") === "true" || false;
+let heightToggleF;
+function functionStart() {
+  toggleBtnForest.style.height = toggleBtnForest.offsetWidth * 0.45 + "px";
+  forestSwitch.style.height = toggleBtnForest.offsetHeight * 0.9 + "px";
+  forestSwitch.style.width = toggleBtnForest.offsetHeight * 0.9 + "px";
+  toggleBtnForest.style.borderRadius = toggleBtnForest.offsetWidth / 2 + "px";
+  forestSwitch.style.transition = "none";
+  bacForest.style.transition = "none";
+  ifIsDark();
+  setTimeout(() => {
+    forestSwitch.style.transition = "transform 1s linear";
+    bacForest.style.transition = "opacity 1s linear";
+  }, 500);
 }
-toggleBtnForest.addEventListener("click", forestOnFunc);
+function forestOnClick() {
+  isDark = !isDark;
+  localStorage.setItem("isDarkForestBTN", isDark);
+  ifIsDark();
+}
+function ifIsDark() {
+  if (isDark) {
+    bacForest.style.opacity = "1";
+    forestSwitch.style.transform = "translate(0, -50%)";
+  } else {
+    bacForest.style.opacity = "0";
+    forestSwitch.style.transform = "translate(100%, -50%)";
+  }
+}
+toggleBtnForest.addEventListener("click", forestOnClick);
+document.addEventListener("DOMContentLoaded", functionStart);
